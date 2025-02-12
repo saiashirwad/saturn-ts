0b097b8d14f757ff9bab509d8a21719493b30a6b
Sai Ashirwad <saiashirwadr@gmail.com>
2025-02-12 12:31:29 +0530

    wip

--- src/utils/hash.ts
+++ src/utils/hash.ts
@@ -1,9 +1,9 @@
+// djb2 algorithm
 export function hashCode(code: string): string {
   let hash = 5381;
   for (let i = 0; i < code.length; i++) {
-    // djb2 algorithm
     hash = (hash << 5) + hash + code.charCodeAt(i);
-    hash = hash >>> 0; // Convert to unsigned 32-bit integer
+    hash = hash >>> 0;
   }
-  return hash.toString(36); // Convert to base36 for shorter string representation
+  return hash.toString(36);
 }
